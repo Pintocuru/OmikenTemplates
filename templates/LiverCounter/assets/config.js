5a8aa6c1600c327a06c266fbaa1767ea8fd0851d
Pintocuru <flyiruka@gmail.com>
2025-05-13 16:08:43 +0900

    250513_1

--- templates/LiverCounter/assets/config.js
+++ templates/LiverCounter/assets/config.js
@@ -1,91 +1,91 @@
 const componentConfig = {
-  "totalCounterSet": null,
-  "isHorizontalLayout": true
+ totalCounterSet: null,
+ isHorizontalLayout: true
 };
 const counterSets = [
-  {
-    "id": "test1",
-    "userVisits": {
-      "IS_DIFF_MODE": false,
-      "ENABLED_SERVICES": "platforms",
-      "ALLOWED_IDS": [],
-      "ACCESS_LEVEL": 1,
-      "IS_GIFT": false,
-      "KEYWORDS": []
-    },
-    "counter": {
-      "component": "BasicCircle",
-      "typeColor": "pink",
-      "title": "初コメ",
-      "unit": "人",
-      "countMode": "syoken",
-      "targetCountdown": 0,
-      "multiplier": 1,
-      "PARTY": {},
-      "PARTY_EVENT": "!syoken",
-      "PARTY_SUCCESS": "",
-      "BOT_NAME": "info",
-      "BOT": {},
-      "BOT_EVENT": "",
-      "BOT_SUCCESS": ""
-    }
+ {
+  id: 'test1',
+  userVisits: {
+   IS_DIFF_MODE: false,
+   ENABLED_SERVICES: 'platforms',
+   ALLOWED_IDS: [],
+   ACCESS_LEVEL: 1,
+   IS_GIFT: false,
+   KEYWORDS: []
   },
-  {
-    "id": "counter-1744322058413",
-    "userVisits": {
-      "IS_DIFF_MODE": false,
-      "ENABLED_SERVICES": "platforms",
-      "ALLOWED_IDS": [],
-      "ACCESS_LEVEL": 1,
-      "IS_GIFT": false,
-      "KEYWORDS": []
-    },
-    "counter": {
-      "component": "BasicCircle",
-      "typeColor": "blue",
-      "title": "リスナー",
-      "unit": "人",
-      "countMode": "user",
-      "targetCountdown": 0,
-      "multiplier": 1,
-      "PARTY": {},
-      "PARTY_EVENT": "",
-      "PARTY_SUCCESS": "",
-      "BOT_NAME": "info",
-      "BOT": {},
-      "BOT_EVENT": "",
-      "BOT_SUCCESS": ""
-    }
-  },
-  {
-    "id": "counter-1744322144895",
-    "userVisits": {
-      "IS_DIFF_MODE": false,
-      "ENABLED_SERVICES": "platforms",
-      "ALLOWED_IDS": [],
-      "ACCESS_LEVEL": 1,
-      "IS_GIFT": false,
-      "KEYWORDS": []
-    },
-    "counter": {
-      "component": "BasicCircle",
-      "typeColor": "green",
-      "title": "コメント",
-      "unit": "",
-      "countMode": "comment",
-      "targetCountdown": 0,
-      "multiplier": 1,
-      "PARTY": {},
-      "PARTY_EVENT": "",
-      "PARTY_SUCCESS": "",
-      "BOT_NAME": "info",
-      "BOT": {},
-      "BOT_EVENT": "",
-      "BOT_SUCCESS": ""
-    }
+  counter: {
+   component: 'BasicCircle',
+   typeColor: 'pink',
+   title: '初コメ',
+   unit: '人',
+   countMode: 'syoken',
+   targetCountdown: 0,
+   multiplier: 1,
+   PARTY: {},
+   PARTY_EVENT: '!syoken',
+   PARTY_SUCCESS: '',
+   BOT_NAME: 'info',
+   BOT: {},
+   BOT_EVENT: '',
+   BOT_SUCCESS: ''
   }
+ },
+ {
+  id: 'counter-1744322058413',
+  userVisits: {
+   IS_DIFF_MODE: false,
+   ENABLED_SERVICES: 'platforms',
+   ALLOWED_IDS: [],
+   ACCESS_LEVEL: 1,
+   IS_GIFT: false,
+   KEYWORDS: []
+  },
+  counter: {
+   component: 'BasicCircle',
+   typeColor: 'blue',
+   title: 'リスナー',
+   unit: '人',
+   countMode: 'user',
+   targetCountdown: 0,
+   multiplier: 1,
+   PARTY: {},
+   PARTY_EVENT: '',
+   PARTY_SUCCESS: '',
+   BOT_NAME: 'info',
+   BOT: {},
+   BOT_EVENT: '',
+   BOT_SUCCESS: ''
+  }
+ },
+ {
+  id: 'counter-1744322144895',
+  userVisits: {
+   IS_DIFF_MODE: false,
+   ENABLED_SERVICES: 'platforms',
+   ALLOWED_IDS: [],
+   ACCESS_LEVEL: 1,
+   IS_GIFT: false,
+   KEYWORDS: []
+  },
+  counter: {
+   component: 'BasicCircle',
+   typeColor: 'green',
+   title: 'コメント',
+   unit: '',
+   countMode: 'comment',
+   targetCountdown: 0,
+   multiplier: 1,
+   PARTY: {},
+   PARTY_EVENT: '',
+   PARTY_SUCCESS: '',
+   BOT_NAME: 'info',
+   BOT: {},
+   BOT_EVENT: '',
+   BOT_SUCCESS: ''
+  }
+ }
 ];
 if (typeof window !== 'undefined') {
-window.componentConfig = componentConfig;
-window.counterSets = counterSets;
+ window.componentConfig = componentConfig;
+ window.counterSets = counterSets;
 }
--- templates/LiverCounter/assets/readme.txt
+++ templates/LiverCounter/assets/readme.txt
@@ -1,11 +1,11 @@
-# ライバービンゴ LiverBingo
+# ライバーカウンター LiverCounter
 
-最終更新日：2025/03/28
+最終更新日：2025/05/13
 
-OBS で使用できる、ライバー・配信者向けのビンゴカードです。
+OBS で使用できる、ライバー・配信者向けの多機能カウンターです。
 ダウンロードして頂きありがとうございます。
 
-導入方法・使用方法については[こちら](https://github.com/Pintocuru/OmikenTemplates/blob/main/docs/WordCounter/readme_WordCounter.md)をご覧ください
+導入方法・使用方法については[こちら](https://github.com/Pintocuru/OmikenTemplates/blob/main/docs/LiverCounter/readme_LiverCounter.md)をご覧ください
 
 ---
 
